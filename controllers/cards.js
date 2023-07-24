73da31aed54113a432eb97d1c297d739de65c8ea
Anna Luneva <aaluneva@edu.hse.ru>
2023-07-24 22:27:19 +0300

    deleted eslintrc.js

--- controllers/cards.js
+++ controllers/cards.js
@@ -34,7 +34,7 @@ const createCard = (req, res) => {
 
   Card.create({ name, link, owner: req.user._id})
   .then((card) => {
-      res.send(card)
+      res.status(201).send(card)
   })
   .catch((err) => {
     if (err.name === 'ValidationError') {
